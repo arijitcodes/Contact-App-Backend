a881ad2598646d09a10c5cbe6d88f9f71a3cfe7a
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-11-21 01:24:27 +0530

    Created REST CRUD Structure of Contacts Route (routes/contacts.js)

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => res.send("Contacts Works!"));
+// Middlewares
+const auth = require("../middlewares/auth");
+
+// @route   GET api/contacts
+// @desc    Get all the contacts of Logged in User
+// @access  Private
+router.get("/", auth, (req, res) => {
+  res.send("GET @ Contacts Works!");
+});
+
+// @route   POST api/contacts
+// @desc    Add a new contact for the Logged in User
+// @access  Private
+router.post("/", auth, (req, res) => {
+  res.send("POST @ Contacts Works!");
+});
+
+// @route   PUT api/contacts
+// @desc    Update a contact of the Logged in User
+// @access  Private
+router.put("/", auth, (req, res) => {
+  res.send("PUT @ Contacts Works!");
+});
+
+// @route   DELETE api/contacts
+// @desc    Delete a contact of the Logged in User
+// @access  Private
+router.delete("/", auth, (req, res) => {
+  res.send("DELETE @ Contacts Works!");
+});
 
 module.exports = router;
