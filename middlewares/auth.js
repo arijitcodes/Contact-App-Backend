d178d51551fff55efd1397d14ef146cf13a9c972
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-11-20 22:06:34 +0530

    Added auth Middleware (middlewares/auth.js)

---
+++ middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+  // Fetch out token from Req. Header
+  const token = req.header("x-auth-token");
+
+  // Checking if token is NOT available.
+  if (!token) {
+    return res.status(401).json({ msg: "Access Denied! No token!" });
+  }
+
+  try {
+    // If token is there, proceed
+    const tokenVerified = jwt.verify(token, config.get("jwtSecret"));
+
+    req.user = tokenVerified;
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: "Invalid Token!" });
+  }
+};
